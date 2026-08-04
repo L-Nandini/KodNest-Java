c8fbe821f418bdd6994a09b2b05aee222a773704
L-Nandini <laknandu123@gmail.com>
2026-08-04 12:24:30 +0530

    Day3 number code change

---
+++ track/Class_Practice/number.java
@@ -0,0 +1,14 @@
+package track.Class_Practice;
+
+public class number {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num == 0) {
+            System.out.println("Number is zero");
+        } else if (num > 0) {
+            System.out.println("Number is positive");
+        } else {
+            System.out.println("Number is negative");
+        }
+    }
+}
