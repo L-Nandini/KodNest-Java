0d2646484f7079cb623969f1218a86b5962e0beb
L-Nandini <laknandu123@gmail.com>
2026-08-05 12:30:18 +0530

    Day4 Pgm_while changes added

---
+++ track/Class_Practice/Pgm_while.java
@@ -0,0 +1,12 @@
+package track.Class_Practice;
+
+public class Pgm_while {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
