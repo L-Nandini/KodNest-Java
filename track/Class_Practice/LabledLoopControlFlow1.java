9d0b416b35b15ca1ea9b45920f4e199281ef0d37
L-Nandini <laknandu123@gmail.com>
2026-08-06 16:06:28 +0530

    WeeklyPracticePlanBuilder is added to practice session

--- track/Class_Practice/LabledLoopControlFlow1.java
+++ track/Class_Practice/LabledLoopControlFlow1.java
@@ -3,7 +3,8 @@ package track.Class_Practice;
 public class LabledLoopControlFlow1 {
     public static void main(String[] args) {
         outer: for (int i = 1; i <= 3; i++) {
-            inner: for (int j = 1; j <= 4; j++) {
+            // inner: for (int j = 1; j <= 4; j++) {
+            for (int j = 1; j <= 4; j++) {
                 if (i == 2) {
                     break outer;
                 }
