7ff223fb0d39b58e2907f1b3a3836de406656421
L-Nandini <laknandu123@gmail.com>
2026-08-10 11:41:34 +0530

    Practice questions added

---
+++ track/PraticeSession/CascadingShiftAfterMixedInput.java
@@ -0,0 +1,21 @@
+package track.PraticeSession;
+
+import java.util.Scanner;
+
+public class CascadingShiftAfterMixedInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        String fullName = scanner.nextLine();
+        String city = scanner.nextLine();
+
+        System.out.println("Name: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+
+        scanner.close();
+    }
+}
