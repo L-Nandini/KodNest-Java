9258666c2b2f0472c6124a6bb279a1349076e7f6
L-Nandini <laknandu123@gmail.com>
2026-08-14 16:01:16 +0530

    Practice class Questions updated

--- track/PracticeSession/Calc_Dis_Report.java
+++ track/PracticeSession/Calc_Dis_Report.java
@@ -1,4 +1,4 @@
-package track.PraticeSession;
+package track.PracticeSession;
 
 public class Calc_Dis_Report {
     public static void main(String[] args) {
---
+++ track/PracticeSession/Passenger.java
@@ -0,0 +1,45 @@
+package track.PraticeSession;
+
+// import java.util.Scanner;
+
+class Conductor {
+    void collect(Money m) {
+        System.out.println("Money collected by Conductor");
+        System.out.println("The amount is : " + m.amount);
+    }
+
+    Ticket give() {
+        Ticket t = new Ticket();
+        System.out.println("Ticket issued");
+        return t;
+    }
+
+}
+
+class Money {
+
+    int amount;
+
+    Money(int amount) {
+        this.amount = amount;
+    }
+}
+
+class Ticket {
+
+}
+
+public class Passenger {
+    public static void main(String[] args) {
+        Conductor c = new Conductor();
+        Money m = new Money(50);
+        Ticket t = c.give();
+        c.collect(m);
+        System.out.println(t);
+        if (t != null) {
+            System.out.println("Enjoy the journey");
+        } else {
+            System.out.println("Please buy ticket");
+        }
+    }
+}
