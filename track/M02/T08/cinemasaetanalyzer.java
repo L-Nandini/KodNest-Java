c87a0ad8de5798f041dbe645b51f9f2682d1f444
L-Nandini <laknandu123@gmail.com>
2026-08-19 13:19:50 +0530

    added practice

---
+++ track/M02/T08/cinemasaetanalyzer.java
@@ -0,0 +1,28 @@
+package track.M02.T08;
+
+import java.util.Scanner;
+
+public class cinemasaetanalyzer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int matrix[][] = new int[r][c];
+        int count = 0;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+
+                if (matrix[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Available seats: " + count);
+        sc.close();
+    }
+}
