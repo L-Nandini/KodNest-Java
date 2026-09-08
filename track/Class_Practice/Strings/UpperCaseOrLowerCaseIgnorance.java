d57975e9c2c5febfa1b00fcdd3b1967017b232df
L-Nandini <laknandu123@gmail.com>
2026-09-08 16:51:00 +0530

    String ignorance case

--- track/Class_Practice/Strings/UpperCaseOrLowerCaseIgnorance.java
+++ track/Class_Practice/Strings/UpperCaseOrLowerCaseIgnorance.java
@@ -1,15 +1,15 @@
 package track.Class_Practice.Strings;
 
-public class String_pgm1_with_new {
+public class UpperCaseOrLowerCaseIgnorance {
     public static void main(String[] args) {
-        String s1 = new String("Java");
-        String s2 = new String("Java");
+        String s1 = "Java";
+        String s2 = "java";
         if (s1 == s2) {
             System.out.println("Ref are Equal");
         } else {
             System.out.println("Ref are Not-Equal");
         }
-        if (s1.equals(s2)) {
+        if (s1.equalsIgnoreCase(s2)) {
             System.out.println("Content are Equal");
         } else {
             System.out.println("Content are Not-Equal");
