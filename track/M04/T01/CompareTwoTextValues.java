d7f8250035ea8640dd429a9416e022f4f0f81951
L-Nandini <laknandu123@gmail.com>
2026-09-08 17:00:44 +0530

    String ignorance case practice question done in another method

--- track/M04/T01/CompareTwoTextValues.java
+++ track/M04/T01/CompareTwoTextValues.java
@@ -9,17 +9,23 @@ public class CompareTwoTextValues {
         String firstText = scanner.nextLine();
         String secondText = scanner.nextLine();
 
+        boolean exactmatch = firstText.equals(secondText);
+        boolean ignorecase = firstText.equalsIgnoreCase(secondText);
+
+        System.out.println("Exact match: " + exactmatch);
+        System.out.println("Ignore-case match: " + ignorecase);
+
         // Compare the two values in both ways.
-        if (firstText.equals(secondText)) {
-            System.out.println("Exact match: true");
-        } else {
-            System.out.println("Exact match: false");
-        }
-        if (firstText.equalsIgnoreCase(secondText)) {
-            System.out.println("Ignore-case match: true");
-        } else {
-            System.out.println("Ignore-case match: false");
-        }
+        // if (firstText.equals(secondText)) {
+        // System.out.println("Exact match: true");
+        // } else {
+        // System.out.println("Exact match: false");
+        // }
+        // if (firstText.equalsIgnoreCase(secondText)) {
+        // System.out.println("Ignore-case match: true");
+        // } else {
+        // System.out.println("Ignore-case match: false");
+        // }
         scanner.close();
     }
 }
