9e8bfecde0f1e204ba37af022837645a45bd1a5b
L-Nandini <laknandu123@gmail.com>
2026-09-09 17:20:42 +0530

    String Practice and HomeWork added

--- track/Class_Practice/Strings/StringConversionAndImmutability.java
+++ track/Class_Practice/Strings/StringConversionAndImmutability.java
@@ -9,26 +9,26 @@ public class StringConversionAndImmutability {
 
         String s2 = "Programming";
         String s3 = s1 + s2;
-        System.out.println(s3);
+        System.out.println(s3); // JavaProgramming
 
         String t1 = new String("Anu");
         String t2 = new String("Bhanu");
         String ans = t1.concat(t2);
-        System.out.println(ans);
+        System.out.println(ans); // AnuBhanu
 
         char[] ch = s1.toCharArray();
-        System.out.println(ch);
+        System.out.println(ch); // [J, a, v, a]
         for (int i = 0; i < ch.length; i++) {
-            System.out.println(ch[i]);
+            System.out.println(ch[i]); // J a v a
         }
         String merge = new String(ch);
-        System.out.println(merge);
+        System.out.println(merge); // Java
 
         byte[] b = s1.getBytes();
         for (int i = 0; i < b.length; i++) {
-            System.out.println(b[i]);
+            System.out.println(b[i]); // 74 97 118 97
         }
         String data = new String(b);
-        System.out.println(data);
+        System.out.println(data); // Java
     }
 }
